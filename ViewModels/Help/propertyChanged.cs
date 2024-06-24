29b83965a072c6697a2791ae5c71349eae624a0c
Lineyss <sascha.ionenko@yandex.ru>
2024-06-24 22:31:05 +0300

    Update

---
+++ ViewModels/Help/propertyChanged.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VFMDesctop.ViewModels.Help
+{
+    class propertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ViewModels/Windows/MainWindowController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Controls;
+using VFMDesctop.View.Pages;
+using VFMDesctop.ViewModels.Help;
+using System.Windows.Media;
+
+namespace VFMDesctop.ViewModels.Windows
+{
+    internal class MainWindowController : propertyChanged
+    {
+        public MainWindowController()
+        {
+            
+        }
+
+        private Page _frameContent = new MainPage();
+        public Page frameContent
+        {
+            get => _frameContent;
+            set
+            {
+                _frameContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Uri _sourceFrame = new Uri("/View/Pages/MainPage.xaml", UriKind.Relative);
+        public Uri sourceFrame
+        {
+            get => _sourceFrame;
+            set
+            {
+                _sourceFrame = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Brush _backgroundButtonMainPage;
+
+        public Brush backgroundButtonMainPage
+        {
+            get => _backgroundButtonMainPage;
+            set
+            {
+                _backgroundButtonMainPage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isEnabledButtonMainPage = false;
+
+        public bool isEnabledButtonMainPage
+        {
+            get => _isEnabledButtonMainPage;
+            set
+            {
+                _isEnabledButtonMainPage = false;
+                OnPropertyChanged(nameof(isEnabledButtonMainPage));
+            }
+        }
+    }
+
+}
